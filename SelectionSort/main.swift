06e3384b284da1084ad3d2e3a48987cdf9da64bd
Omer Erturk <omer.erturk@student.allenisd.org>
2022-10-07 21:25:02 -0500

    selection

--- SelectionSort/main.swift
+++ SelectionSort/main.swift
@@ -7,11 +7,13 @@ let unsortedIntegers = [5, 1, 4, 2, 8]
 
 
 // Add your code below:
-
-var a = unsortedIntegers
+var g : [String] = []
+while let line = readLine() {
+    g.append(line)
+}
+var a = g
 var z = 0
 
-print("Pass: \(0), Swaps: \(0)/\(0), Array: \(a)") 
 for x in 0..<a.count-1{
     var min = x
     for y in x+1..<a.count{
@@ -23,5 +25,5 @@ for x in 0..<a.count-1{
     }
     z+=1
     a.swapAt(x, min)
-    print("Pass: \(x+1), Swaps: \(1)/\(z), Array: \(a)")
+   
 }
